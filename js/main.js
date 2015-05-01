95110725b0c5e69d313f0a19577e39ee9b21a4b9
ornette coleman <omniosi@gmail.com>
2015-05-01 11:03:55 -0700

    updated header image match to width.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 (function(){
 	var init = function(){
+		var under = document.getElementById('under');
 		var over = document.getElementById('over');
 		var hdr = document.getElementsByTagName('header')[0];
 
+		function resize(){
+			// under.style.height = over.style.height = hdr.offsetHeight + 'px';
+			under.style.width = over.style.width = hdr.offsetWidth + 'px';
+		}
+		// match img height to header
+		resize();
+
+		// thunder animation on rollover
 		hdr.addEventListener("mouseenter", function(event){
 			over.classList.add("fade");
 		});
