f122aacf94da7eba6b3eace0a762cffdaeaf7566
ornette coleman <omniosi@gmail.com>
2015-05-01 13:49:41 -0700

    functional setup of section reveal.

--- js/main.js
+++ js/main.js
@@ -3,13 +3,26 @@
 		var under = document.getElementById('under');
 		var over = document.getElementById('over');
 		var hdr = document.getElementsByTagName('header')[0];
+		var oneB = document.getElementById('oneB');
+		var oneF = document.getElementById('oneF');
+		var intro = document.getElementById('intro');
+		var start = document.getElementById('start');
 
 		function resize(){
 			// under.style.height = over.style.height = hdr.offsetHeight + 'px';
 			under.style.width = over.style.width = hdr.offsetWidth + 'px';
 		}
+		function artHide(){
+			// when a belt section is clicked hide the intro articles and reveal the matching text
+			intro.classList.add('hide');
+			start.classList.add('hide');
+		}
+		function artShow(){
+			// when the close button is clicked hide the full text and restore intro articles
+			intro.classList.remove('hide');
+			start.classList.remove('hide');
+		}
 		// match img height to header
-		// window.onresize = resize();
 		window.onresize = resize;
 		resize();
 
@@ -19,6 +32,20 @@
 		});
 		hdr.addEventListener("mouseout", function(event){
 			over.classList.remove("fade");
+		});
+
+		// when a belt section is clicked hide the intro articles and reveal the matching text
+		oneB.addEventListener('click', function(){
+			artHide();
+			oneF.classList.remove('off');
+			oneF.classList.add('on');
+
+			// var close = oneF.getElementsByTagName('close')[0];
+			oneF.addEventListener('click',function(){
+				oneF.classList.remove('on');
+				oneF.classList.add('off');
+				artShow();
+			});
 		});
 	};
 	init();
