f055f82e952c20ed70e7889a29a4072399569cde
ornette coleman <omniosi@gmail.com>
2015-05-01 09:55:38 -0700

    fade animation complete.

--- js/main.js
+++ js/main.js
@@ -3,18 +3,12 @@
 		var over = document.getElementById('over');
 		var hdr = document.getElementsByTagName('header')[0];
 
-		// over.style.height = hdr.offsetHeight + 'px';
-
-		// hdr.on hover = over.style.display:block;
 		hdr.addEventListener("mouseenter", function(event){
-			over.style.opacity = 1;
-
-			// fade out after a short delay
-			setTimeout(function(){
-				over.style.opacity = 0;
-			},500);
+			over.classList.add("fade");
 		});
-
+		hdr.addEventListener("mouseout", function(event){
+			over.classList.remove("fade");
+		});
 	};
 	init();
 })();
